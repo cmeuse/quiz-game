2a35e6a44c0bba3d11dc0b08ca869b8e1b391284
cmeuse <chloemeuse@gmail.com>
2021-08-12 14:09:05 -0700

    changes7

--- quiz.js
+++ quiz.js
@@ -1,4 +1,49 @@
+(function(){
 
+function buildQuiz(){
+  const output=[];
+  questions.forEach((currentQ, qNum) => {
+    const answers = [];
+    for(letter in curretQ.answers){
+      answers.push(
+         `<label>
+           <input type="radio" name="question${qNum}" value="${letter}">
+           ${letter} :
+           ${currentQ.answers[letter]}
+         </label>`
+       );
+    }
+    output.push(
+        `<div class="question"> ${currentQ.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+  }
+);
+  quizContainer.innerHTML = output.join('');
+}
+
+
+function showResults(){
+   const answerContainers = quizContainer.querySelectorAll('.answers');
+   let numCorrect = 0;
+   questions.forEach( (currentQ, qNum) => {
+     const answerContainer = answerContainers[qNum];
+    const selector = `input[name=question${qNum}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    if(userAnswer === currentQ.correctAnswer){
+      numCorrect++;
+      answerContainers[qNum].style.color = 'lightgreen';
+    }
+    else{
+      answerContainers[qNum].style.color = 'red';
+    }
+  });
+  resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
+}
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 const questions = [
   {
     question: "When was Tagger founded?",
@@ -42,54 +87,9 @@ const questions = [
   }
   }
 ];
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
 
 
-function buildQuiz(){
-  const output=[];
-  questions.forEach((currentQ, qNum) => {
-    const answers = [];
-    for(letter in curretQ.answers){
-      answers.push(
-         `<label>
-           <input type="radio" name="question${qNum}" value="${letter}">
-           ${letter} :
-           ${currentQ.answers[letter]}
-         </label>`
-       );
-    }
-    output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-      );
-  }
-);
-  quizContainer.innerHTML = output.join('');
-}
-
-
-function showResults(){
-   const answerContainers = quizContainer.querySelectorAll('.answers');
-   let numCorrect = 0;
-   myQuestions.forEach( (currentQ, qNum) => {
-     const answerContainer = answerContainers[qNum];
-    const selector = `input[name=question${qNum}]:checked`;
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-    if(userAnswer === currentQ.correctAnswer){
-      numCorrect++;
-      answerContainers[qNum].style.color = 'lightgreen';
-    }
-    else{
-      answerContainers[qNum].style.color = 'red';
-    }
-  });
-  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-}
-
-
-}
 
 buildQuiz();
 submitButton.addEventListener('click', showResults);
+}
