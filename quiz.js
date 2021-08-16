1e339b0915748af17ced2015a2eaf98466234af1
cmeuse <84832341+cmeuse@users.noreply.github.com>
2021-08-16 11:50:15 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -71,7 +71,7 @@ function showProgress() {
 function showScores() {
     var gameOverHTML = "<h1>Result</h1>";
     let percent = (quiz.score/quiz.questions.length) * 100;
-    gameOverHTML += "<h2 id='score'> Your score: " + quiz.percent + "% </h2>";
+    gameOverHTML += "<h2 id='score'> Your score: " + percent + "% </h2>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
